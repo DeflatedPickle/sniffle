adfd6dc305d3f6cf56754a7405f981f8f91d66b6
DeflatedPickle <DeflatedPickle@gmail.com>
2020-08-08 20:51:40 +0100

    Stole some plugins from Rawky

---
+++ pluginmanager/src/main/kotlin/com/deflatedpickle/rawky/pluginmanager/PluginManager.kt
@@ -0,0 +1,74 @@
+package com.deflatedpickle.rawky.pluginmanager
+
+import com.deflatedpickle.haruhi.api.constants.MenuCategory
+import com.deflatedpickle.haruhi.api.plugin.Plugin
+import com.deflatedpickle.haruhi.api.plugin.PluginType
+import com.deflatedpickle.haruhi.event.EventMenuBuild
+import com.deflatedpickle.haruhi.event.EventProgramFinishSetup
+import com.deflatedpickle.haruhi.event.EventWindowShown
+import com.deflatedpickle.haruhi.util.PluginUtil
+import com.deflatedpickle.haruhi.util.RegistryUtil
+import com.deflatedpickle.tosuto.ToastItem
+import com.deflatedpickle.tosuto.action.ToastSingleAction
+import com.deflatedpickle.tosuto.api.ToastLevel
+import javax.swing.JMenu
+
+@Plugin(
+    value = "plugin_manager",
+    author = "DeflatedPickle",
+    version = "1.0.0",
+    description = """
+        <br>
+        A GUI for managing plugins
+    """,
+    type = PluginType.DIALOG
+)
+@Suppress("unused")
+object PluginManager {
+    init {
+        EventProgramFinishSetup.addListener {
+            val menuBar = RegistryUtil.get(MenuCategory.MENU.name)
+            val toolMenu = menuBar?.get(MenuCategory.TOOLS.name) as JMenu
+            toolMenu.add("Plugin Manager").apply {
+                addActionListener {
+                    PluginManagerDialog.isVisible = true
+                }
+            }
+        }
+
+        EventWindowShown.addListener { window ->
+            if (window != PluginUtil.toastWindow) return@addListener
+
+            if (PluginUtil.loadedPlugins.isNotEmpty()) {
+                PluginUtil.toastWindow.addToast(
+                    ToastItem(
+                        title = "Loaded Plugins",
+                        content = PluginUtil.loadedPlugins.joinToString {
+                            PluginUtil.pluginToSlug(it)
+                        },
+                        actions = listOf(
+                            ToastSingleAction(
+                                "Manage",
+                                command = { _, _ ->
+                                    PluginManagerDialog.isVisible = true
+                                }
+                            )
+                        )
+                    )
+                )
+            }
+
+            if (PluginUtil.unloadedPlugins.isNotEmpty()) {
+                PluginUtil.toastWindow.addToast(
+                    ToastItem(
+                        level = ToastLevel.WARNING,
+                        title = "Unloaded Plugins",
+                        content = PluginUtil.unloadedPlugins.joinToString {
+                            it.value
+                        }
+                    )
+                )
+            }
+        }
+    }
+}
---
+++ settingsgui/src/main/kotlin/com/deflatedpickle/rawky/settings/SettingsGUI.kt
@@ -0,0 +1,52 @@
+package com.deflatedpickle.rawky.settings
+
+import com.deflatedpickle.haruhi.api.constants.MenuCategory
+import com.deflatedpickle.haruhi.api.plugin.Plugin
+import com.deflatedpickle.haruhi.event.EventProgramFinishSetup
+import com.deflatedpickle.haruhi.util.PluginUtil
+import com.deflatedpickle.haruhi.util.RegistryUtil
+import java.util.regex.PatternSyntaxException
+import javax.swing.JMenu
+import javax.swing.tree.DefaultMutableTreeNode
+
+@Plugin(
+    value = "settings_gui",
+    author = "DeflatedPickle",
+    version = "1.0.0",
+    dependencies = [
+        "deflatedpickle@core#1.0.0",
+        "deflatedpickle@discord_rpc#1.0.0"
+    ]
+)
+object SettingsGUI {
+    init {
+        EventProgramFinishSetup.addListener {
+            val menuBar = RegistryUtil.get(MenuCategory.MENU.name)
+            val toolMenu = menuBar?.get(MenuCategory.FILE.name) as JMenu
+            toolMenu.add("Settings").apply {
+                addActionListener {
+                    SettingsDialog.isVisible = true
+                }
+            }
+        }
+
+        EventProgramFinishSetup.addListener {
+            for (plugin in PluginUtil.discoveredPlugins) {
+                if (plugin.settings != Nothing::class) {
+                    SettingsDialog.searchPanel.model.insertNodeInto(
+                        DefaultMutableTreeNode(PluginUtil.pluginToSlug(plugin)),
+                        Categories.nodePlugin,
+                        SettingsDialog.searchPanel.model.getChildCount(Categories.nodePlugin)
+                    )
+                }
+            }
+        }
+
+        SettingsDialog.searchPanel.searchField.addActionListener {
+            try {
+                SettingsDialog.searchPanel.tree.searchable.search(it.actionCommand)
+            } catch (e: PatternSyntaxException) {
+            }
+        }
+    }
+}
