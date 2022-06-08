d7aed62bda85c66610ef5145cfa45ec63505eb83
DeflatedPickle <DeflatedPickle@gmail.com>
2022-06-08 18:01:38 +0100

    Added a range on font sizes

--- swingsettings/src/main/kotlin/com/deflatedpickle/sniffle/swingsettings/api/Font.kt
+++ swingsettings/src/main/kotlin/com/deflatedpickle/sniffle/swingsettings/api/Font.kt
@@ -2,6 +2,7 @@
 
 package com.deflatedpickle.sniffle.swingsettings.api
 
+import com.deflatedpickle.rawky.settings.api.Range
 import com.deflatedpickle.rawky.settings.widget.ConfigSection
 import kotlinx.serialization.Required
 import kotlinx.serialization.Serializable
@@ -11,5 +12,5 @@ import javax.swing.UIManager
 data class Font(
     @Required var name: FontFamily = FontFamily(UIManager.getDefaults().getFont("TabbedPane.font").family),
     @Required var style: FontStyle = FontStyle.PLAIN,
-    @Required var size: Int = 12
+    @Required @Range(1, 36) var size: Int = 12
 ) : ConfigSection
